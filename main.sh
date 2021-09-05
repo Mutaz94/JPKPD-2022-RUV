05153a48b611170a84522ef9554c11b6c4563b4e
Mutaz <mutazjab94@gmail.com>
2021-09-05 15:34:08 -0500

    Linux/MacOS

---
+++ main.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/sh
+
+echo ------------------------------------------------------- \n
+echo Investigating the contribution of residual unexplained \n
+echo variability components in NLME approach \n
+echo ------------------------------------------------------- \n
