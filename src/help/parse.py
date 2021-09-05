8b22411aa765e00c26efff4a2b465da8e51f2f05
Mutaz <mutazjab94@gmail.com>
2021-09-05 14:05:41 -0500

    script to parse nm models

---
+++ src/help/parse.py
@@ -0,0 +1,13 @@
+#--!encoding-utf8
+"""
+Project: Investigating the contribution of residual unexplaind variability components in NLME approach
+Program: Create NONMEM files and attach them to corresponding dataset
+Author: Mutaz M. Jaber <jaber038@umn.edu>
+Date created: 9/5/21
+Date modified: 9/5/21
+"""
+
+import numpy
+import os
+import sys
+import string
