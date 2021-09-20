fb91e861ca4423a980d6c9e04b412b6ac7766b54
Mutaz Jaber <mutazjab94@gmail.com>
2021-09-20 14:28:36 -0500

    Program to clean NONMEM garbage

---
+++ src/help/clean.py
@@ -0,0 +1,8 @@
+
+"""
+Project: Investigating the contribution of residual unexplaind variability components in NLME approach
+Program: Clean nonmem garbage 
+Author: Mutaz M. Jaber <jaber038@umn.edu>
+Date created: 9/20/21
+Date modified: 9/20/21
+"""
