5bd7544a96aa3106d418c92b0341e50c5b659e2f
Mutaz Jaber <mutazjab94@gmail.com>
2021-10-01 10:03:40 -0500

    final main

--- src/sim/generate.R
+++ src/sim/generate.R
@@ -246,7 +246,7 @@ GetData <- function(model,
         	
                for (i in 1:nsim) {
                  		 set.seed(i) 
-		                 DOSE = round(rnorm(nsubs, DOSE, 12),0) 
+		                 DOSE = rnorm(nsubs, DOSE, 12)
 		                 event <- ev(amt=DOSE, time=TDOSE, ID=1:nsubs, replicate=F)
 		                 
 		                 set.seed(i) 
