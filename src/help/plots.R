070a6e59aa9811d5499edc5bcdbaec64aed2daea
Mutaz Jaber <mutazjab94@gmail.com>
2021-10-06 18:12:14 -0500

    add figures

--- src/help/plots.R
+++ src/help/plots.R
@@ -31,8 +31,8 @@ plot.1 <- ggplot(DAT[DAT$X=='RUV',], aes(per, log(Mean), group=design))+
         geom_line(aes(color=design), size=.75)+
         geom_errorbar(aes(ymin=log(X95CIlo), ymax=log(X95CIup)), width=0.2)+
         labs(color='Study design', x='Perturbation', y =expression(log(Deviation)), title=expression(RUV))+
-        scale_x_discrete(limits=c('B', 'A1', 'A2', 'A3', 'D', 'S1', 'SL1', 'SL2', 'SL3','TD1', 'TD2', 'All'), 
-        labels=c('Base', expression(A[1]), expression(A[2]), expression(A[3]), expression(D[v]), 
+        scale_x_discrete(limits=c('B', 'A1', 'A2', 'A3', 'M', 'D', 'S1', 'SL1', 'SL2', 'SL3','TD1', 'TD2', 'All'), 
+        labels=c('Base', expression(A[1]), expression(A[2]), expression(A[3]), expression(M), expression(D[v]), 
                   expression(S[5]), expression(S[10]), expression(S[15]), expression(S[30]), 
                   expression(D[t5]), expression(D[t10]), expression(All)))+
         scale_color_discrete(limits=c('SD1', 'SD3', 'SD2', 'SD4'),
@@ -58,8 +58,44 @@ plot.3 <- ggplot(DAT[DAT$X=='RUV',], aes(per, rRMSE*100, group=design))+
                   expression(D[t5]), expression(D[t10]), expression(All)))+
         scale_color_discrete(labels=c(expression(SD[8]), expression(SD[6]), expression(SD[5]), expression(SD[4]))) 
 
+plot.4 <- ggplot(DAT[DAT$X=='CL',], aes(per, log(Mean), group=design))+
+        geom_point(aes(color=design), size=1)+
+        geom_line(aes(color=design), size=.75)+
+        geom_errorbar(aes(ymin=log(X95CIlo), ymax=log(X95CIup)), width=0.2)+
+        labs(color='Study design', x='Perturbation', y =expression(log(Deviation)), title=expression(CL))+
+        scale_x_discrete(limits=c('B', 'A1', 'A2', 'A3', 'D', 'S1', 'SL1', 'SL2', 'SL3','TD1', 'TD2', 'All'), 
+        labels=c('Base', expression(A[1]), expression(A[2]), expression(A[3]), expression(D[v]), 
+                  expression(S[5]), expression(S[10]), expression(S[15]), expression(S[30]), 
+                  expression(D[t5]), expression(D[t10]), expression(All)))+
+        scale_color_discrete(limits=c('SD1', 'SD3', 'SD2', 'SD4'),
+                             labels=c(expression(SD[9]), expression(SD[6]), expression(SD[5]), expression(SD[4]))) 
+plot.5 <- ggplot(DAT[DAT$X=='V',], aes(per, log(Mean), group=design))+
+        geom_point(aes(color=design), size=1)+
+        geom_line(aes(color=design), size=.75)+
+        geom_errorbar(aes(ymin=log(X95CIlo), ymax=log(X95CIup)), width=0.2)+
+        labs(color='Study design', x='Perturbation', y =expression(log(Deviation)), title=expression(V))+
+        scale_x_discrete(limits=c('B', 'A1', 'A2', 'A3', 'D', 'S1', 'SL1', 'SL2', 'SL3','TD1', 'TD2', 'All'), 
+        labels=c('Base', expression(A[1]), expression(A[2]), expression(A[3]), expression(D[v]), 
+                  expression(S[5]), expression(S[10]), expression(S[15]), expression(S[30]), 
+                  expression(D[t5]), expression(D[t10]), expression(All)))+
+        scale_color_discrete(limits=c('SD1', 'SD3', 'SD2', 'SD4'),
+                             labels=c(expression(SD[9]), expression(SD[6]), expression(SD[5]), expression(SD[4]))) 
+plot.6 <- ggplot(DAT[DAT$X=='Q',], aes(per, log(Mean), group=design))+
+        geom_point(aes(color=design), size=1)+
+        geom_line(aes(color=design), size=.75)+
+        geom_errorbar(aes(ymin=log(X95CIlo), ymax=log(X95CIup)), width=0.2)+
+        labs(color='Study design', x='Perturbation', y =expression(log(Deviation)), title=expression(Q))+
+        scale_x_discrete(limits=c('B', 'A1', 'A2', 'A3', 'D', 'S1', 'SL1', 'SL2', 'SL3','TD1', 'TD2', 'All'), 
+        labels=c('Base', expression(A[1]), expression(A[2]), expression(A[3]), expression(D[v]), 
+                  expression(S[5]), expression(S[10]), expression(S[15]), expression(S[30]), 
+                  expression(D[t5]), expression(D[t10]), expression(All)))+
+        scale_color_discrete(limits=c('SD1', 'SD3', 'SD2', 'SD4'),
+                             labels=c(expression(SD[9]), expression(SD[6]), expression(SD[5]), expression(SD[4]))) 
 pdf('RUV-JPKPD-Figures.pdf', width=12, height=8)
 plot.1
 plot.2
 plot.3
+plot.4
+plot.5
+plot.6
 dev.off()
